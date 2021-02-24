256abe9c465e54ef12d2eb356611eb7090561210
BauyrzhanAzimkhanov <57823983+BauyrzhanAzimkhanov@users.noreply.github.com>
2021-02-24 14:08:08 +0600

    lab4

---
+++ lab4/tutorial/src/app/products.ts
@@ -0,0 +1,101 @@
+export const products = [
+  {
+    id: 1,
+    name: 'AMD Ryzen 5 3600',
+    price: 199.99,
+    description: '6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler',
+    link: 'https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=sr_1_3?dchild=1&keywords=AMD+Ryzen+5+2600&qid=1614084450&sr=8-3',
+    rating: '4.9 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/71WPGXQLcLL._AC_SL1384_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71zVPc8ItVL._AC_SL1384_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/716hAjT1uUL._AC_SL1500_.jpg']
+  },
+  {
+    id: 2,
+    name: 'G.Skill RipJaws V Series 16GB (2 x 8GB)',
+    price: 99.99,
+    description: 'PC4-28800 DDR4 3600 CL16-19-19-39 1.35V Dual Channel Desktop Memory Model',
+    link: 'https://www.amazon.com/G-Skill-RipJaws-PC4-28800-CL16-19-19-39-F4-3600C16D-16GVKC/dp/B07X8DVDZZ/ref=sr_1_3?dchild=1&keywords=G.Skill+RipJaws+V+Series+16GB+%282+x+8GB%29&qid=1614084347&sr=8-3',
+    rating: '4.8 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/61jrb3gMvxL._AC_SX679_PIbundle-2,TopRight,0,0_SH20_.jpg', '', '']
+  },
+  {
+    id: 3,
+    name: 'MSI B450 TOMAHAWK Max',
+    price: 114.99,
+    description: 'AM4 M.2 USB 3 DDR4 DVI HDMI Crossfire ATX Motherboard',
+    link: 'https://www.amazon.com/MSI-Crossfire-Motherboard-Tomahawk-B450TOMAMAX/dp/B07WF6ZQST/ref=sr_1_1?dchild=1&keywords=MSI+B450+TOMAHAWK+Max&qid=1614084316&sr=8-1',
+    rating: '4.8 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/914pic6PuAL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/61eyIyNx%2BVL._AC_SL1024_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/81Zu0K2dMBL._AC_SL1500_.jpg']
+  },
+  {
+    id: 4,
+    name: 'Seasonic FOCUS GX-750',
+    price: 174.99,
+    description: '750W 80+ Gold, Full-Modular, Power Supply',
+    link: 'https://www.amazon.com/Seasonic-GX-750-Full-Modular-Application-SSR-750FX/dp/B077J9G9CH/ref=sr_1_1?dchild=1&keywords=Seasonic+FOCUS+GX-750&qid=1614084292&sr=8-1',
+    rating: '4.7 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/61un-RQ7zvL._AC_SL1267_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71Wxfmai77L._AC_SL1200_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71zb9QD%2BNOL._AC_SL1200_.jpg']
+  },
+  {
+    id: 4,
+    name: 'Noctua NH-D15',
+    price: 89.95,
+    description: 'CPU Cooler with 2X NF-A15 PWM 140mm Fans (Brown)',
+    link: 'https://www.amazon.com/Noctua-NH-D15-heatpipe-NF-A15-140mm/dp/B00L7UZMAK/ref=sr_1_4?dchild=1&keywords=Noctua+NH-D15&qid=1614084268&sr=8-4',
+    rating: '4.8 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/91Hw1zcAIjL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/81pZdedh3EL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71ysNICiDNL._AC_SL1500_.jpg']
+  },
+  {
+    id: 5,
+    name: 'ARCTIC MX-5',
+    price: 11.99,
+    description: '(8 g, Incl. Spatula) Thermal Paste',
+    link: 'https://www.amazon.com/dp/B08T6KYVWP/ref=twister_B08WMT79XK?_encoding=UTF8&psc=1',
+    rating: '4.8 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/51Gxitk2GML._AC_SL1200_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/614IZPynDVL._AC_SL1200_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/61S7ybKQGeL._AC_SL1200_.jpg']
+  },
+  {
+    id: 6,
+    name: 'Seagate BarraCuda 2TB',
+    price: 54.99,
+    description: 'Internal Hard Drive HDD – 3.5 Inch SATA 6Gb/s 7200 RPM 256MB Cache 3.5-Inch',
+    link: 'https://www.amazon.com/Seagate-BarraCuda-Internal-Drive-3-5-Inch/dp/B07H2RR55Q/ref=sr_1_3?dchild=1&keywords=Seagate+BarraCuda+2TB&qid=1614084212&sr=8-3',
+    rating: '4.7 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/71Czt9ypIbL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/6165QqoVhgL._AC_SL1000_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71N8t-BZvVL._AC_SL1500_.jpg']
+  },
+  {
+    id: 7,
+    name: 'Samsung 970 PRO SSD 512GB',
+    price: 128.67,
+    description: 'M.2 NVMe Interface Internal Solid State Drive with V-NAND Technology',
+    link: 'https://www.amazon.com/Samsung-SSD-970-PRO-512GB/dp/B07C8Y31G2/ref=sr_1_1?dchild=1&keywords=Samsung+970+PRO+SSD+512GB&qid=1614084172&sr=8-1',
+    rating: '4.8 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/81fadiYUPtL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/81MZSHjuCRL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/61eRJpYsLWL._AC_SL1000_.jpg']
+  },
+  {
+    id: 8,
+    name: 'DEEPCOOL MATREXX 70',
+    price: 54.99,
+    description: 'Mid-Tower Computer Case',
+    link: 'https://www.amazon.com/MATREXX-70-Mid-Tower-Full-Size-Installation/dp/B07QVBCW31/ref=sr_1_1?dchild=1&keywords=DEEPCOOL+MATREXX+70&qid=1614084148&sr=8-1',
+    rating: '4.4 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/61WLHjQ5lnL._AC_SL1000_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/51%2BsDWCVSYL._AC_SL1000_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/61V8CPPcyZL._AC_SL1000_.jpg']
+  },
+  {
+    id: 9,
+    name: 'GIGABYTE GeForce RTX 2080 Ti',
+    price: 1699.95,
+    description: '11GB Graphic Cards',
+    link: 'https://www.amazon.com/GIGABYTE-GeForce-Graphic-GV-N208TGAMING-OC-11GC/dp/B07YLDH9KT/ref=sr_1_16?dchild=1&qid=1614083271&refinements=p_n_feature_four_browse-bin%3A16955282011%7C6066318011&rnid=2057431011&s=computers-intl-ship&sr=1-16',
+    rating: '3.4 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/81S13Vyv73L._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71JQ6b-I5vL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71FDOmyGsqL._AC_SL1500_.jpg']
+  },
+  {
+    id: 10,
+    name: 'LG 27QN600-B 27” QHD (2560 x 1440)',
+    price: 279.00,
+    description: 'IPS Display with FreeSync, HDR10',
+    link: 'https://www.amazon.com/LG-27QN600-B-FreeSync-Virtually-Borderless/dp/B089NZCLHW/ref=sr_1_5?dchild=1&keywords=hdr&qid=1614090161&refinements=p_n_size_browse-bin%3A3547807011%2Cp_n_feature_fourteen_browse-bin%3A17751789011%2Cp_n_feature_twelve_browse-bin%3A17726586011&rnid=17726583011&s=pc&sr=1-5',
+    rating: '4.7 out of 5',
+    images: ['https://images-na.ssl-images-amazon.com/images/I/91nWAV%2BB9%2BL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/71jIeq7SsGL._AC_SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/81%2BVCGiIiXL._AC_SL1500_.jpg']
+  }
+];
