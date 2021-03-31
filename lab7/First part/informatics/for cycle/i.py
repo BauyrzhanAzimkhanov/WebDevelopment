b068b8a5d7d9d18ca076c9a727e72b71d3b2231d
Bauyrzhan <azimkhanov4@gmail.com>
2021-03-31 18:24:52 +0600

    Final
    I hope that is!

--- lab7/First part/informatics/for cycle/i.py
+++ lab7/First part/informatics/for cycle/i.py
@@ -1,8 +1,8 @@
 import math
-a = int(input())
-ans = 0
-for i in range(1, math.ceil(a / 2) + 1):
-    if(a % i == 0):
-        #print(i)
-        ans += 1
-print(ans + 1, end="")
+n = int(input())
+k = 0
+for i in range(1, math.floor(math.sqrt(n + 1))):
+    if n%i==0:
+        k+=2
+ 
+print(k)
